0f4fc738440109a7d3b658c429000a2b9cca59ce
Balaji21s <chandabalaji338@gmail.com>
2025-06-17 15:45:07 +0530

    Create FirstNonRepeatingCharacter.java

---
+++ FirstNonRepeatingCharacter.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class FirstNonRepeatingCharacter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+
+        Map<Character, Integer> count = new LinkedHashMap<>();
+        for (char c : s.toCharArray()) {
+            count.put(c, count.getOrDefault(c, 0) + 1);
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (count.get(s.charAt(i)) == 1) {
+                System.out.println(i);
+                return;
+            }
+        }
+
+        System.out.println(-1);
+    }
+}
